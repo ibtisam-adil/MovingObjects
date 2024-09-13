84f41d738d8e7457bfa2f53a3dbc91e2c80e4246
ibtisam adil <ibtisamadil273@gmail.com>
2024-09-13 20:32:04 +0200

    Conditional change of direction of the ball

--- Source/main.cpp
+++ Source/main.cpp
@@ -19,30 +19,40 @@ void game_init()
 }
 
 void update() {
+	int window_width = get_window_width();
+
 	if (is_key_pressed(Key::Space))
 	{
 		john.x_facing_directions = -john.x_facing_directions;
 	}
+
 	john.x_position_in_pixels += john.x_facing_directions * SQUARE_X_SPEED_PIXELS;
+
+	if (john.x_position_in_pixels >= window_width - SQUARE_SIZE_PIXELS) 
+	{
+		john.x_facing_directions = -1.0f;
+	}
+	else if (john.x_position_in_pixels <= 0.0f) 
+	{
+		john.x_facing_directions = 1.0f;
+	}
 }
 
 void render()
 {
-	// INFO: Edit this code
-
-	int window_width  = get_window_width();
+	//int window_width  = get_window_width();
 	int window_height = get_window_height();
 
 	int square_x = round_to_int(john.x_position_in_pixels);
 	int square_y = round_to_int(window_height * 0.5f);
 	draw_rectangle(square_x, square_y, SQUARE_SIZE_PIXELS, SQUARE_SIZE_PIXELS);
+
 }
 
 int main()
 {
+	game_init();
 	initialize(720, 720, "Window");
-
-	// Add code here (before the while-loop) if it only needs to run once at the beginning.
 
 	while (is_window_open())
 	{
